185c887f33864662e5d251b0f48110d70f37c698
Joseph <2342934D@student.gla.ac.uk>
2018-01-21 17:56:01 +0000

    end of chapter 4

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Path to the templates directory using BASE_DIR
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+# Path to the static directory using BASE_DIR
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+# Path to the media directory using BASE_DIR
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,7 +63,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -118,4 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# A list of paths with which Django can expect to find static files
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+#URL to serve static files from
 STATIC_URL = '/static/'
+
+# A list of paths with which Django can expect to find uploaded media files
+MEDIA_ROOT = MEDIA_DIR
+
+# URL to serve uploaded media files from
+MEDIA_URL = '/media/'
